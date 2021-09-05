36d5e4a789c91d9bea3f3353c29df4f5b29e5a23
odeivissonsantos <deivissonsantos@hotmail.com>
2021-09-05 11:08:14 -0300

    inicializacao do projeto

---
+++ src/main/java/com/odeivissonsantos/checklistapp/ApiCheckListApplication.java
@@ -0,0 +1,13 @@
+package com.odeivissonsantos.checklistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCheckListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCheckListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odeivissonsantos/checklistapp/ApiCheckListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeivissonsantos.checklistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCheckListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
